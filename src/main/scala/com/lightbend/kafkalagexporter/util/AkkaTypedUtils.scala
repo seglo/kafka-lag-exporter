069908c936418d793ba418ebdf0e560cd8803c6a
Sean Glover <sean@seanglover.com>
2019-01-09 14:16:59 -0500

    Akka logging receive utility

---
+++ src/main/scala/com/lightbend/kafkalagexporter/util/AkkaTypedUtils.scala
@@ -0,0 +1,16 @@
+package com.lightbend.kafkalagexporter.util
+
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.{ActorContext, Behaviors}
+import akka.actor.typed.scaladsl.Behaviors.Receive
+
+package object AkkaTypedUtils {
+  /**
+    * Log typed message to DEBUG and passthrough to receive handler.
+    */
+  def loggingReceive[T](onMessage: PartialFunction[(ActorContext[T], T), Behavior[T]]): Receive[T] =
+    Behaviors.receive[T] { (ctx, t) ⇒
+      ctx.log.debug(s"Received: $t")
+      onMessage.applyOrElse((ctx, t), (_: (ActorContext[T], T)) ⇒ Behaviors.unhandled[T])
+    }
+}
