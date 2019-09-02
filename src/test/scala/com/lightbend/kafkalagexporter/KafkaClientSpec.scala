76f28a96e3cb41c03ec49011024575c69ee47911
Sean Glover <sean@seanglover.com>
2019-09-02 14:38:01 -0400

    Get commit offset for correct group topic partitions (#56)

--- src/test/scala/com/lightbend/kafkalagexporter/KafkaClientSpec.scala
+++ src/test/scala/com/lightbend/kafkalagexporter/KafkaClientSpec.scala
@@ -9,13 +9,12 @@ import java.util.Optional
 import com.lightbend.kafkalagexporter.Domain.GroupOffsets
 import com.lightbend.kafkalagexporter.KafkaClient.KafkaTopicPartitionOps
 import org.apache.kafka.clients.consumer.OffsetAndMetadata
-import org.apache.kafka.common.KafkaFuture
 import org.mockito.MockitoSugar
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FreeSpec, Matchers}
 
 import scala.collection.JavaConverters._
-import scala.concurrent.{ExecutionContext, ExecutionContextExecutor}
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
 import scala.concurrent.duration.FiniteDuration
 
 class KafkaClientSpec extends FreeSpec with Matchers with TestData with MockitoSugar with ScalaFutures {
@@ -37,29 +36,57 @@ class KafkaClientSpec extends FreeSpec with Matchers with TestData with MockitoS
 
       val client = spy(new KafkaClient(cluster, groupId, FiniteDuration(0, "ms")))
 
-      val offset = new OffsetAndMetadata(1, Optional.empty(), "")
-
-      val groupId0Results = KafkaFuture.completedFuture(Map(
-        topicPartition0.asKafka -> offset
+      val groupId0Results = Future.successful(Map(
+        topicPartition0.asKafka -> new OffsetAndMetadata(0, Optional.empty(), "")
         // missing topicPartition1
         // missing topicPartition2
       ).asJava)
       doReturn(groupId0Results).when(client).getListConsumerGroupOffsets(groupId0)
 
-      val groupId1Results = KafkaFuture.completedFuture(Map(
-        topicPartition0.asKafka -> offset
+      val groupId1Results = Future.successful(Map(
+        topicPartition0.asKafka -> new OffsetAndMetadata(1, Optional.empty(), "")
       ).asJava)
 
       doReturn(groupId1Results).when(client).getListConsumerGroupOffsets(groupId1)
 
+
       val groupOffsets = client.getGroupOffsets(0, groups, gtps).futureValue
 
-      groupOffsets shouldEqual GroupOffsets(
-        gtp0_0 -> Some(LookupTable.Point(1, 0)),
-        gtp1_0 -> None, // missing partition
-        gtp2_0 -> None, // missing partition
-        gtp0_1 -> Some(LookupTable.Point(1, 0))
-      )
+      groupOffsets.size shouldEqual 4
+      groupOffsets(gtp0_0) shouldEqual Some(LookupTable.Point(0, 0))
+      groupOffsets(gtp1_0) shouldEqual None // missing partition
+      groupOffsets(gtp2_0) shouldEqual None // missing partition
+      groupOffsets(gtp0_1) shouldEqual Some(LookupTable.Point(1, 0))
+
+    }
+
+    "getGroupOffsets returns distinct offsets when multiple groups subscribe to same partitions" in {
+      val groupId0 = "testGroupId0"
+      val groupId1 = "testGroupId1"
+
+      val groups = List(groupId0, groupId1)
+
+      val gtp0_0 = gtp0.copy(id = groupId0)
+      val gtp0_1 = gtp0.copy(id = groupId1)
+
+      val gtps = List(gtp0_0, gtp0_1)
+
+      val client = spy(new KafkaClient(cluster, groupId, FiniteDuration(0, "ms")))
+
+      val groupId0Results = Future.successful(Map(
+        topicPartition0.asKafka -> new OffsetAndMetadata(0, Optional.empty(), "")
+      ).asJava)
+      doReturn(groupId0Results).when(client).getListConsumerGroupOffsets(groupId0)
+
+      val groupId1Results = Future.successful(Map(
+        topicPartition0.asKafka -> new OffsetAndMetadata(1, Optional.empty(), "")
+      ).asJava)
+      doReturn(groupId1Results).when(client).getListConsumerGroupOffsets(groupId1)
+
+      val groupOffsets = client.getGroupOffsets(0, groups, gtps).futureValue
+
+      groupOffsets(gtp0_0) shouldEqual Some(LookupTable.Point(0, 0))
+      groupOffsets(gtp0_1) shouldEqual Some(LookupTable.Point(1, 0))
     }
 
     "getOffsetOrZero returns offsets of None (Option[Point]) for missing partitions" in {
