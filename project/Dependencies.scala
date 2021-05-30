7c9cc972b7640cdcef353c01f32f2a97aec13de0
Scala Steward <me@scala-steward.org>
2021-05-31 00:22:50 +0200

    Update simpleclient, simpleclient_hotspot, ... to 0.11.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Version {
   val Scala          = "2.12.11"
   val Akka           = "2.6.11"
-  val Prometheus     = "0.8.1"
+  val Prometheus     = "0.11.0"
   val Fabric8        = "4.9.1"
   val Kafka          = "2.5.0"
   val Testcontainers = "1.15.1"
