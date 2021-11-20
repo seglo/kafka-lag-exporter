062799b62d9d22ee4037bcf724926282562d4edf
Sean Glover <sean@seanglover.com>
2021-11-20 14:15:27 -0500

    fix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -43,8 +43,8 @@ object Dependencies {
   val AkkaTypedTestKit       = "com.typesafe.akka"       %% "akka-actor-testkit-typed"  % Version.Akka           % Test
   val ScalaTest              = "org.scalatest"           %% "scalatest"                 % "3.1.4"                % Test
   val AkkaStreamsTestKit     = "com.typesafe.akka"       %% "akka-stream-testkit"       % Version.Akka           % Test
-  val MockitoScala           = "org.mockito"             %% "mockito-scala"             % "1.0.8"                % Test
-  val AlpakkaKafkaTestKit    = "com.typesafe.akka"       %% "akka-stream-kafka-testkit" % "2.0.6+3-255156cc"     % Test excludeAll(jacksonExclusionRule, log4jExclusionRule, slf4jExclusionRule)
+  val MockitoScala           = "org.mockito"             %% "mockito-scala"             % "1.0.10"               % Test
+  val AlpakkaKafkaTestKit    = "com.typesafe.akka"       %% "akka-stream-kafka-testkit" % "2.0.7"                % Test excludeAll(jacksonExclusionRule, log4jExclusionRule, slf4jExclusionRule)
   val TestcontainersKafka    = "org.testcontainers"      %  "kafka"                     % Version.Testcontainers % Test
   val TestcontainersInfluxDb = "org.testcontainers"      %  "influxdb"                  % Version.Testcontainers % Test
 }
