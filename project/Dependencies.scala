12d98e3be5c055dc54332fc1df6659d2e23effbe
Sverre H Huseby <shh@thathost.com>
2022-08-21 16:44:10 +0200

    Update Kafka

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Version {
   val Akka = "2.6.19"
   val Prometheus = "0.16.0"
   val Fabric8 = "4.9.1"
-  val Kafka = "2.5.0"
+  val Kafka = "3.2.1"
   val Testcontainers = "1.17.3"
   val IAMAuth = "1.1.4"
 }
