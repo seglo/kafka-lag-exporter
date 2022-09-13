e62f0137331a7898a323cbe2b11bbfb5b38d9e26
Scala Steward <scala_steward@virtuslab.com>
2022-09-13 15:38:40 +0000

    Revert commit(s) 34bcbcd

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Version {
   val Scala = "2.12.16"
   val Akka = "2.6.19"
   val Prometheus = "0.15.0"
-  val Fabric8 = "4.13.3"
+  val Fabric8 = "4.11.2"
   val Kafka = "3.2.1"
   val Testcontainers = "1.17.3"
   val IAMAuth = "1.1.4"
